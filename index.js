882d4b790cc2dbc9dd8924205abeea5d97dc0f7d
Stephen <stephen@tdsoftwareservices.com>
2023-03-08 01:03:37 +0800

    Updated cors

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(cors());
 
 // Allows us to communicate between different domain
 app.use((req, res, next) => {
@@ -44,6 +43,9 @@ app.use((req, res, next) => {
 });
 
 /** SOCKET IO */
+
+app.use(cors());
+
 const server = http.createServer(app);
 let activeUsers = [];
 
